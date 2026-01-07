6f3ff4ceecff25c576024988f828e84dda30adab
jandesson-moraes <jandessonmoraes@gmail.com>
2026-01-07 01:22:19 -0300

    Atualização v6.1

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,7 @@ import {
   MessageCircle, DollarSign, Loader2,
   Lock, KeyRound, ChevronRight, BellRing, ClipboardCopy, FileText,
   Trash2, Edit, Wallet, Calendar, MinusCircle, ArrowDownCircle, ArrowUpCircle,
-  Camera, LayoutDashboard, Map as MapIcon, CheckCircle2, AlertCircle
-} from 'lucide-react';
+  Camera, LayoutDashboard, Map as MapIcon, } from 'lucide-react';
 
 // --- FIREBASE IMPORTS ---
 import { initializeApp } from "firebase/app";
